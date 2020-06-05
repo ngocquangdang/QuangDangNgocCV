caa915d32492590e9ada853b2ff0186717b86c17
Quang (Basil) N. DANG <basil@enclave.vn>
2020-06-05 16:11:36 +0700

    Updates

---
+++ precache-manifest.2742da8fb42294d5858425efa6466bf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9063d28421d6b6d133831e57e15926a1",
+    "url": "/Qprofile/index.html"
+  },
+  {
+    "revision": "d1fad866b327526bb100",
+    "url": "/Qprofile/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bdb2e968372506323b41",
+    "url": "/Qprofile/static/js/2.47f69619.chunk.js"
+  },
+  {
+    "revision": "590e481f934ff291c2e7632daa063457",
+    "url": "/Qprofile/static/js/2.47f69619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1fad866b327526bb100",
+    "url": "/Qprofile/static/js/main.5ee5a0b5.chunk.js"
+  },
+  {
+    "revision": "a5fd94ca9c54aa6df51c",
+    "url": "/Qprofile/static/js/runtime-main.793f0eaa.js"
+  }
+]);
