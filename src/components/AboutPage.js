2041baff11e30077ac823b73119306cbaf7f9a46
Quang (Basil) N. DANG <basil@enclave.vn>
2020-06-05 17:02:16 +0700

    create layout about page

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -4,11 +4,28 @@ import styled from 'styled-components';
 export default function AboutPage(){
   return (
     <Container id='about-page'>
-      <p>About Page</p>
+      <Avatar>
+        <div> avatar</div>
+      </Avatar>
+      <About>
+        <div>about</div>
+      </About>
     </Container>
   )
 }
 
 const Container =  styled.div`
+  display: flex;
+  background: gray;
+  padding: 100px 150px;
+  width: 100vh;
   height: 100vh;
+`
+const Avatar = styled.div`
+   flex: 4;
+   background: yellow;
+`
+const About = styled.div`
+  flex: 5;
+  background: green;
 `
